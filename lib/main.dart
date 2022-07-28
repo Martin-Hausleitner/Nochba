36d838588a433307bb298b136e1467336778425f
Martin Hausleitner <55828102+Martin-Hausleitner@users.noreply.github.com>
2022-07-28 20:30:50 +0200

    add themes

--- lib/main.dart
+++ lib/main.dart
@@ -18,32 +18,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final TextTheme textTheme = TextTheme(
-      headline1: GoogleFonts.inter(
-          fontSize: 97, fontWeight: FontWeight.w300, letterSpacing: -1.5),
-      headline2: GoogleFonts.inter(
-          fontSize: 61, fontWeight: FontWeight.w300, letterSpacing: -0.5),
-      headline3: GoogleFonts.inter(fontSize: 48, fontWeight: FontWeight.w400),
-      headline4: GoogleFonts.inter(
-          fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-      headline5: GoogleFonts.inter(fontSize: 24, fontWeight: FontWeight.w400),
-      headline6: GoogleFonts.inter(
-          fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: 0.15),
-      subtitle1: GoogleFonts.inter(
-          fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
-      subtitle2: GoogleFonts.inter(
-          fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
-      bodyText1: GoogleFonts.inter(
-          fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
-      bodyText2: GoogleFonts.inter(
-          fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-      button: GoogleFonts.inter(
-          fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
-      caption: GoogleFonts.inter(
-          fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
-      overline: GoogleFonts.inter(
-          fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
-    );
+    final TextTheme textTheme = _textTheme;
 
     return MaterialApp(
       theme: ThemeData(
