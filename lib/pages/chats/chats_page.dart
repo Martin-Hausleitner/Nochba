57f1ed7eb266e763702be6792b2cc2583d4925e1
Edalatkhah <arsham.edal@gmail.com>
2022-08-07 00:43:13 +0200

    Add Chat funktion

--- lib/pages/chats/chats_page.dart
+++ lib/pages/chats/chats_page.dart
@@ -2,19 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'chats_controller.dart';
+import 'rooms.dart';
 
 class ChatsPage extends GetView<ChatsController> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text(
-            "chats",
-            style: TextStyle(fontSize: 20),
-          ),
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Firebase Chat',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-      ),
-    );
-  }
+        home: const RoomsPage(),
+      );
 }
