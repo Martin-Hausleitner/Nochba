b07b0d9ba9c2855b1c7e18d5da540e083d1db83d
Martin <s.m@hausleitner.eu>
2022-08-16 21:41:50 +0200

    smaller changes

---
+++ lib/views/feed/action_bar.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import '../../shared/round_icon_button.dart';
+
+//create a ActionBar class which have multiple round icon Buttons
+
+class ActionBar extends StatelessWidget {
+  const ActionBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // return a row with round icon buttons and a text
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children:  [
+        // return a round icon button with a icon of Icons.favorite and a color of Colors.red
+        RoundIconButton(
+          icon: Icons.thumb_up,
+          iconColor: Colors.black,
+          onPressed: () {
+            Get.snackbar(
+              "Edit",
+              "Edit your profile",
+            );
+          },
+        ),
+        // add a gray Text
+        Padding(
+          padding: EdgeInsets.only(left: 8, right: 18),
+          child: Text(
+            '14',
+            style: TextStyle(
+              fontSize: 13,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey,
+            ),
+          ),
+        ),
+
+        // return a round icon button with a icon of Icons.favorite and a color of Colors.red
+        RoundIconButton(
+          icon: Icons.forum,
+          iconColor: Colors.black,
+          onPressed: () {
+            Get.snackbar(
+              "Edit",
+              "Edit your profile",
+            );
+          },
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 8, right: 18),
+          child: Text(
+            '14',
+            style: TextStyle(
+              fontSize: 13,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey,
+            ),
+          ),
+        ),
+        // return a round icon button with a icon of Icons.favorite and a color of Colors.red
+        RoundIconButton(
+          icon: Icons.bookmark,
+          iconColor: Colors.black,
+          onPressed: () {
+            Get.snackbar(
+              "Edit",
+              "Edit your profile",
+            );
+          },
+        ),
+        Spacer(),
+        RoundIconButton(
+          icon: Icons.more_horiz,
+          iconColor: Colors.black,
+          onPressed: () {
+            Get.snackbar(
+              "Edit",
+              "Edit your profile",
+            );
+          },
+        ),
+
+        // add a row with aliged left with a RoundIconButton
+
+        // return a round icon button with a icon of Icons.favorite and a color of Colors.red
+      ],
+    );
+  }
+}
---
+++ lib/views/feed/discription.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Discription extends StatelessWidget {
+  const Discription({
+    Key? key,
+    required this.postDescription,
+  }) : super(key: key);
+
+  final String postDescription;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: const Alignment(1, 1),
+      children: [
+        //align text left
+        Align(
+          alignment: Alignment.centerLeft,
+          child: SelectableText(
+            //print postDescription.split('\n').length as string
+            postDescription,
+            
+            
+            
+
+            maxLines: 3,
+            style: GoogleFonts.inter(
+              //change the space bettwene the letters
+              letterSpacing: -0.1,
+              fontSize: 14,
+              color: Colors.grey.shade800,
+            ),
+          ),
+        ),
+
+        SizedBox(
+          height: 12,
+          child: Row(
+            // alighn left
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                width: 17,
+                //height is a s bit as the other container
+                height: 50,
+
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.white.withOpacity(0.3),
+                      Colors.white,
+                    ],
+                  ),
+                ),
+              ),
+              //create a white container with a text as child
+              Container(
+                height: 50,
+                padding: const EdgeInsets.only(left: 6),
+                // create a white background
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: const Text(
+                  'More',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        // Container(
+        //   padding: const EdgeInsets.only(left: 20),
+        //   decoration: const BoxDecoration(
+        //     gradient: LinearGradient(
+        //       begin: Alignment.topLeft,
+        //       end: Alignment.bottomRight,
+        //       colors: [
+        //         Color.fromARGB(144, 221, 221, 221),
+        //         Colors.white,
+        //         Colors.blue,
+        //       ],
+        //     ),
+        //   ),
+        //   child: const Text(
+        //     'Read More',
+        //     style: TextStyle(
+        //       fontSize: 14,
+        //       fontWeight: FontWeight.w500,
+        //       color: Colors.black,
+        //     ),
+        //   ),
+        // ),
+      ],
+    );
+  }
+}
