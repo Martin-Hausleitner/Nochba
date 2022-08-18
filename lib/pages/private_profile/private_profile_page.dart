6fba7513f63568fd877693506b1a229d8f0d0c67
Martin <s.m@hausleitner.eu>
2022-08-18 23:00:55 +0200

    add posts

--- lib/pages/private_profile/private_profile_page.dart
+++ lib/pages/private_profile/private_profile_page.dart
@@ -8,8 +8,11 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:locoo/shared/round_icon_button.dart';
 
 import '../../views/settings/settings_page.dart';
+import '../feed/post/category_badge.dart';
 import 'private_profile_controller.dart';
 import 'package:contained_tab_bar_view/contained_tab_bar_view.dart';
+// import post.dart
+import '../../pages/feed/post/post.dart';
 
 class PrivateProfilePage extends GetView<PrivateProfileController> {
   final List<Tab> myTabs = <Tab>[
@@ -99,7 +102,6 @@ class PrivateProfilePage extends GetView<PrivateProfileController> {
     // );
     // return Scaffold( with a colum and a row with headline5 text and a settings icon
     return Scaffold(
-      
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('Dein Profil', style: TextStyle(color: Colors.black)),
@@ -192,29 +194,69 @@ class PrivateProfilePage extends GetView<PrivateProfileController> {
             ),
           ),
           //
+          SizedBox(
+            height: 20,
+          ),
 
           // add a tabbarview
-          Container(
-                width: width,
-                height: height,
-                child: ContainedTabBarView(
-                  tabs: [
-                    Text('Info', style: TextStyle(fontSize: 20)),
-                    Text('Posts', style: TextStyle(fontSize: 20)),
-                  ],
-                  tabBarProperties: TabBarProperties(
-                    indicatorColor: Theme.of(context).primaryColor,
-                    indicatorWeight: 8.0,
-                    labelColor: Theme.of(context).primaryColor,
-                    unselectedLabelColor: Colors.grey[400],
-                  ),
-                  views: [
-                    Container(color: Colors.red),
-                    Container(color: Colors.green),
-                  ],
-                  onChange: (index) => print(index),
-                ),
+          Expanded(
+            // width: width,
+            // height: height,
+            child: ContainedTabBarView(
+              tabs: [
+                Text('Info', style: TextStyle(fontSize: 20)),
+                Text('Posts', style: TextStyle(fontSize: 20)),
+              ],
+              tabBarProperties: TabBarProperties(
+                indicatorColor: Theme.of(context).primaryColor,
+                indicatorWeight: 4.0,
+                labelColor: Theme.of(context).primaryColor,
+                unselectedLabelColor: Colors.grey[400],
               ),
+              views: [
+                Container(color: Colors.red.shade100),
+                // create a container with a list of post.dart
+                Container(
+                  color: Color.fromARGB(146, 238, 238, 238),
+                  child: ListView(children: [
+                    SizedBox(height: 6),
+                    Post(
+                      postTitle: 'Post ddddddddddddTitle',
+                      postImage: 'https://i.pravatar.cc/303',
+                      postCategory: Category.mitteilung,
+                      postAuthorName: 'John Doe',
+                      postPublishDate: '2',
+                      postDistance: '1',
+                      postDescription: 'Post Dddddescrisssption',
+                    ),
+                    SizedBox(height: 3),
+                    Post(
+                      postTitle: 'Post Title',
+                      postImage: 'https://i.pravatar.cc/303',
+                      postAuthorName: 'John Doe',
+                      postPublishDate: '2',
+                      postDistance: '1',
+                      postDescription:
+                          'Post https://pub.dev/packages/expandable_text Dddddescrdfffffffffffffffdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfffffffffffffffffffffffffffffffffffffffffisf dddddddddddsdffffffffffffsdfffffdddption',
+                    ),
+                    SizedBox(height: 3),
+                    Post(
+                      postTitle:
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                      postImage: 'https://i.pravatar.cc/303',
+                      postAuthorName: 'John Doe',
+                      postPublishDate: '2',
+                      postCategory: Category.warnung,
+                      postDistance: '1',
+                      postDescription:
+                          'Post https://pub.dev/packages/expandable_text Dddddescrdfffffffffffffffdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfffffffffffffffffffffffffffffffffffffffffisf dddddddddddsdffffffffffffsdfffffdddption',
+                    ),
+                  ]),
+                ),
+              ],
+              onChange: (index) => print(index),
+            ),
+          ),
           // add TabBarView with with info and post
         ],
       ),
