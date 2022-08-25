934525f46b40a80597e48374420e2b7283399926
Martin <s.m@hausleitner.eu>
2022-08-25 21:47:23 +0200

    chnage appbar chats

--- lib/pages/chats/chats_page.dart
+++ lib/pages/chats/chats_page.dart
@@ -1,9 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_firebase_chat_core/flutter_firebase_chat_core.dart';
+import 'package:flutter_remix/flutter_remix.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'chat.dart';
@@ -23,7 +27,6 @@ class _ChatsPageState extends State<ChatsPage> {
   bool _error = false;
   bool _initialized = false;
   User? _user;
-  
 
   @override
   void initState() {
@@ -40,143 +43,281 @@ class _ChatsPageState extends State<ChatsPage> {
     if (!_initialized) {
       return Container();
     }
-
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            pinned: true,
-            snap: true,
-            floating: true,
-            shadowColor: Colors.transparent,
-            backgroundColor: Colors.white,
-            expandedHeight: 160.0,
-            forceElevated: true,
-            actions: [
-              // IconButton(
-              //   icon: const Icon(FlutterRemix.pencil_line),
-              //   color: Colors.black,
-              //   onPressed: () {
-              //     Get.snackbar('dsfdsf', 'dsfdsf');
-              //   },
-              // ),
-              IconButton(
-                icon: const Icon(Icons.add, color: Colors.black),
-                onPressed: _user == null
-                    ? null
-                    : () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            fullscreenDialog: true,
-                            builder: (context) => const UsersPage(),
-                          ),
-                        );
-                      },
-              ),
-              IconButton(
-                icon: const Icon(
-                  Icons.logout,
-                  color: Colors.black,
-                ),
-                onPressed: _user == null ? null : logout,
-              ),
-            ],
-            flexibleSpace: FlexibleSpaceBar(
-                title: Text(
-                  'Nachrichten',
-                  style: GoogleFonts.inter(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w800,
-                    letterSpacing: -0.5,
-                    color: Color.fromARGB(255, 22, 22, 22),
-                  ),
-                ),
-                titlePadding: EdgeInsets.only(left: 15, bottom: 15)),
+    return CupertinoPageScaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      // A ScrollView that creates custom scroll effects using slivers.
+      child: CupertinoTheme(
+        data: CupertinoThemeData(
+          textTheme: CupertinoTextThemeData(
+            navTitleTextStyle:
+                Theme.of(context).textTheme.headlineSmall?.copyWith(
+                      // fontFamily: 'Inter',
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      // letterSpacing: -0.5,
+                      color: Theme.of(context).colorScheme.onSecondaryContainer,
+                    ),
+            navLargeTitleTextStyle:
+                Theme.of(context).textTheme.headlineMedium?.copyWith(
+                      // fontFamily: 'Inter',
+                      fontSize: 30,
+                      fontWeight: FontWeight.w800,
+                      letterSpacing: -0.5,
+                      color: Theme.of(context).colorScheme.onSecondaryContainer,
+                    ),
           ),
-          SliverFillRemaining(
-            child: _user == null
-                ? Container(
-                    alignment: Alignment.center,
-                    margin: const EdgeInsets.only(
-                      bottom: 200,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text('Not authenticated'),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                fullscreenDialog: true,
-                                builder: (context) => const LoginPage(),
-                              ),
-                            );
-                          },
-                          child: const Text('Login'),
-                        ),
-                      ],
-                    ),
-                  )
-                : StreamBuilder<List<types.Room>>(
-                    stream: FirebaseChatCore.instance.rooms(),
-                    initialData: const [],
-                    builder: (context, snapshot) {
-                      if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                        return Container(
-                          alignment: Alignment.center,
-                          margin: const EdgeInsets.only(
-                            bottom: 200,
-                          ),
-                          child: const Text('No rooms'),
-                        );
-                      }
+          // barBackgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+          barBackgroundColor: Theme.of(context).backgroundColor,
+        ),
+        child: CustomScrollView(
+          // A list of sliver widgets.
+          slivers: <Widget>[
+            CupertinoSliverNavigationBar(
+              // padding: EdgeInsetsDirectional.only(
+              //   start: 7,
+              //   end: 23,
+              // ),
 
-                      return ListView.builder(
-                        itemCount: snapshot.data!.length,
-                        itemBuilder: (context, index) {
-                          final room = snapshot.data![index];
+              // leading: Icon(CupertinoIcons.person_2),
+              // This title is visible in both collapsed and expanded states.
+              // When the "middle" parameter is omitted, the widget provided
+              // in the "largeTitle" parameter is used instead in the collapsed state.
+              largeTitle: Text(
+                'Nachrichten',
+              ),
 
-                          return GestureDetector(
-                            onTap: () {
+              trailing: Container(
+                  transform: Matrix4.translationValues(14, 0, 0),
+                  child: IconButton(
+                    // alignment: Alignment.topRight,
+                    icon: Icon(
+                      FlutterRemix.pencil_line,
+                      size: 24,
+                      color: Theme.of(context).colorScheme.onSecondaryContainer,
+                    ),
+                    onPressed: () {
+                      Get.snackbar('title', 'message');
+                    },
+                  )),
+              border: //make the border transparent
+                  const Border(bottom: BorderSide(color: Colors.transparent)),
+            ),
+            // This widget fills the remaining space in the viewport.
+            // Drag the scrollable area to collapse the CupertinoSliverNavigationBar.
+            SliverFillRemaining(
+              child: _user == null
+                  ? Container(
+                      alignment: Alignment.center,
+                      margin: const EdgeInsets.only(
+                        bottom: 200,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text('Not authenticated'),
+                          TextButton(
+                            onPressed: () {
                               Navigator.of(context).push(
                                 MaterialPageRoute(
-                                  builder: (context) => ChatPage(
-                                    room: room,
-                                  ),
+                                  fullscreenDialog: true,
+                                  builder: (context) => const LoginPage(),
                                 ),
                               );
                             },
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 16,
-                                vertical: 8,
-                              ),
-                              child: Row(
-                                children: [
-                                  _buildAvatar(room),
-                                  Text(room.name ?? ''),
-                                ],
-                              ),
+                            child: const Text('Login'),
+                          ),
+                        ],
+                      ),
+                    )
+                  : StreamBuilder<List<types.Room>>(
+                      stream: FirebaseChatCore.instance.rooms(),
+                      initialData: const [],
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                          return Container(
+                            alignment: Alignment.center,
+                            margin: const EdgeInsets.only(
+                              bottom: 200,
                             ),
+                            child: const Text('No rooms'),
                           );
-                        },
-                      );
-                    },
-                  ),
-          ),
-        ],
+                        }
+
+                        return ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            final room = snapshot.data![index];
+
+                            return GestureDetector(
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => ChatPage(
+                                      room: room,
+                                    ),
+                                  ),
+                                );
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 16,
+                                  vertical: 8,
+                                ),
+                                child: Row(
+                                  children: [
+                                    _buildAvatar(room),
+                                    Text(room.name ?? ''),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
+  //   return Scaffold(
+  //     body: CustomScrollView(
+  //       slivers: <Widget>[
+  //         SliverAppBar(
+  //           pinned: true,
+  //           snap: true,
+  //           floating: true,
+  //           shadowColor: Colors.transparent,
+  //           backgroundColor: Colors.white,
+  //           expandedHeight: 160.0,
+  //           forceElevated: true,
+  //           actions: [
+  //             // IconButton(
+  //             //   icon: const Icon(FlutterRemix.pencil_line),
+  //             //   color: Colors.black,
+  //             //   onPressed: () {
+  //             //     Get.snackbar('dsfdsf', 'dsfdsf');
+  //             //   },
+  //             // ),
+  //             IconButton(
+  //               icon: const Icon(Icons.add, color: Colors.black),
+  //               onPressed: _user == null
+  //                   ? null
+  //                   : () {
+  //                       Navigator.of(context).push(
+  //                         MaterialPageRoute(
+  //                           fullscreenDialog: true,
+  //                           builder: (context) => const UsersPage(),
+  //                         ),
+  //                       );
+  //                     },
+  //             ),
+  //             IconButton(
+  //               icon: const Icon(
+  //                 Icons.logout,
+  //                 color: Colors.black,
+  //               ),
+  //               onPressed: _user == null ? null : logout,
+  //             ),
+  //           ],
+  //           flexibleSpace: FlexibleSpaceBar(
+  //               title: Text(
+  //                 'Nachrichten',
+  //                 style: GoogleFonts.inter(
+  //                   fontSize: 24,
+  //                   fontWeight: FontWeight.w800,
+  //                   letterSpacing: -0.5,
+  //                   color: Color.fromARGB(255, 22, 22, 22),
+  //                 ),
+  //               ),
+  //               titlePadding: EdgeInsets.only(left: 15, bottom: 15)),
+  //         ),
+  // SliverFillRemaining(
+  //   child: _user == null
+  //       ? Container(
+  //           alignment: Alignment.center,
+  //           margin: const EdgeInsets.only(
+  //             bottom: 200,
+  //           ),
+  //           child: Column(
+  //             mainAxisAlignment: MainAxisAlignment.center,
+  //             children: [
+  //               const Text('Not authenticated'),
+  //               TextButton(
+  //                 onPressed: () {
+  //                   Navigator.of(context).push(
+  //                     MaterialPageRoute(
+  //                       fullscreenDialog: true,
+  //                       builder: (context) => const LoginPage(),
+  //                     ),
+  //                   );
+  //                 },
+  //                 child: const Text('Login'),
+  //               ),
+  //             ],
+  //           ),
+  //         )
+  //       : StreamBuilder<List<types.Room>>(
+  //           stream: FirebaseChatCore.instance.rooms(),
+  //           initialData: const [],
+  //           builder: (context, snapshot) {
+  //             if (!snapshot.hasData || snapshot.data!.isEmpty) {
+  //               return Container(
+  //                 alignment: Alignment.center,
+  //                 margin: const EdgeInsets.only(
+  //                   bottom: 200,
+  //                 ),
+  //                 child: const Text('No rooms'),
+  //               );
+  //             }
+
+  //             return ListView.builder(
+  //               itemCount: snapshot.data!.length,
+  //               itemBuilder: (context, index) {
+  //                 final room = snapshot.data![index];
+
+  //                 return GestureDetector(
+  //                   onTap: () {
+  //                     Navigator.of(context).push(
+  //                       MaterialPageRoute(
+  //                         builder: (context) => ChatPage(
+  //                           room: room,
+  //                         ),
+  //                       ),
+  //                     );
+  //                   },
+  //                   child: Container(
+  //                     padding: const EdgeInsets.symmetric(
+  //                       horizontal: 16,
+  //                       vertical: 8,
+  //                     ),
+  //                     child: Row(
+  //                       children: [
+  //                         _buildAvatar(room),
+  //                         Text(room.name ?? ''),
+  //                       ],
+  //                     ),
+  //                   ),
+  //                 );
+  //               },
+  //             );
+  //           },
+  //         ),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
+
   void initializeFlutterFire() async {
     try {
       if (Firebase.apps.isEmpty) {
-      await Firebase.initializeApp(
-        name: "THIS",
-        options: DefaultFirebaseOptions.currentPlatform,
-      );
+        await Firebase.initializeApp(
+          name: "THIS",
+          options: DefaultFirebaseOptions.currentPlatform,
+        );
       }
       FirebaseAuth.instance.authStateChanges().listen((User? user) {
         setState(() {
