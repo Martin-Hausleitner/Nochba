aacfa3f4829e229677f448315954b9522641f112
Martin <s.m@hausleitner.eu>
2022-08-25 20:23:32 +0200

    lol

--- lib/pages/feed/feed_page.dart
+++ lib/pages/feed/feed_page.dart
@@ -6,7 +6,6 @@ import 'package:locoo/pages/feed/post/post.dart';
 import 'package:locoo/models/post.dart' as models;
 import 'package:locoo/models/user.dart' as models;
 
-
 import '../../shared/range_slider/range_slider.dart';
 import 'feed_controller.dart';
 
@@ -19,15 +18,14 @@ class FeedPage extends GetView<FeedController> {
         child: StreamBuilder<List<models.Post>>(
           stream: dataAccess.getPosts(),
           builder: (context, snapshot) {
-            if(snapshot.hasError) {
-                return Text('Something went wrong: ${snapshot.error.toString()}',
-                    textAlign: TextAlign.center, 
-                    style: const TextStyle(fontSize: 32, fontWeight: FontWeight.w300)
-                  );
-
-            } else if(snapshot.hasData) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong: ${snapshot.error.toString()}',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontSize: 32, fontWeight: FontWeight.w300));
+            } else if (snapshot.hasData) {
               final posts = snapshot.data!;
-              
+
               return ListView.separated(
                 shrinkWrap: true,
                 itemCount: posts.length,
@@ -37,8 +35,7 @@ class FeedPage extends GetView<FeedController> {
                   return FutureBuilder<models.User?>(
                     future: dataAccess.getUser(post.user),
                     builder: ((context, snapshot) {
-
-                      if(snapshot.hasData) {
+                      if (snapshot.hasData) {
                         final user = snapshot.data!;
                         return Post(
                           post: post,
@@ -50,58 +47,19 @@ class FeedPage extends GetView<FeedController> {
                       }
                     }),
                   );
-                }, 
-                separatorBuilder: (BuildContext context, int index)  => const SizedBox(height: 3),
+                },
+                separatorBuilder: (BuildContext context, int index) =>
+                    const SizedBox(height: 3),
               );
-
             } else {
               return const Center(child: CircularProgressIndicator());
               // return const Text('There are no posts in the moment',
-              //   textAlign: TextAlign.center, 
+              //   textAlign: TextAlign.center,
               //   style: TextStyle(fontSize: 32, fontWeight: FontWeight.w300));
-            } 
+            }
           },
-        )
-        
-        
-        
-        /*ListView(
-          children: const [
-            Slider1(),
-            Post(
-              postTitle: 'Post ddddddddddddTitle',
-              postImage: 'https://i.pravatar.cc/303',
-              postCategory: Category.mitteilung,
-              postAuthorName: 'John Doe',
-              postPublishDate: '2',
-              postDistance: '1',
-              postDescription: 'Post Dddddescrisssption',
-            ),
-            SizedBox(height: 3),
-            Post(
-              postTitle: 'Post Title',
-              postImage: 'https://i.pravatar.cc/303',
-              postAuthorName: 'John Doe',
-              postPublishDate: '2',
-              postDistance: '1',
-              postDescription:
-                  'Post https://pub.dev/packages/expandable_text Dddddescrdfffffffffffffffdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfffffffffffffffffffffffffffffffffffffffffisf dddddddddddsdffffffffffffsdfffffdddption',
-            ),
-            SizedBox(height: 3),
-            Post(
-              postTitle:
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-              postImage: 'https://i.pravatar.cc/303',
-              postAuthorName: 'John Doe',
-              postPublishDate: '2',
-              postCategory: Category.warnung,
-              postDistance: '1',
-              postDescription:
-                  'Post https://pub.dev/packages/expandable_text Dddddescrdfffffffffffffffdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfffffffffffffffffffffffffffffffffffffffffisf dddddddddddsdffffffffffffsdfffffdddption',
-            ),
-          ],
-        ),*/
-          ),
+        ),
+      ),
     );
   }
 }
--- lib/shared/profile_content.dart
+++ lib/shared/profile_content.dart
@@ -5,7 +5,18 @@ import '../pages/feed/post/category_badge.dart';
 import '../pages/feed/post/post.dart';
 // import '../pages/private_profile/small_post/small_post.dart';
 import 'package:contained_tab_bar_view/contained_tab_bar_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:locoo/models/category.dart';
+import 'package:locoo/models/data_access.dart';
+import 'package:locoo/pages/feed/post/post.dart';
+import 'package:locoo/models/post.dart' as models;
+import 'package:locoo/models/user.dart' as models;
 
+import '../../shared/range_slider/range_slider.dart';
+import 'package:locoo/pages/feed/feed_controller.dart';
+
+//import feed Controller
 
 class ProfileContent extends StatelessWidget {
   const ProfileContent({
@@ -14,6 +25,8 @@ class ProfileContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dataAccess = Get.find<DataAccess>();
+
     return Expanded(
       // width: width,
       // height: height,
@@ -38,37 +51,54 @@ class ProfileContent extends StatelessWidget {
             color: Color.fromARGB(146, 238, 238, 238),
             child: ListView(children: [
               SizedBox(height: 6),
-              // SmallPost(
-              //   postTitle: 'Post ddddddddddddTitle',
-              //   postImage: 'https://i.pravatar.cc/303',
-              //   // postCategory: Category.mitteilung,
-              //   postAuthorName: 'John Doe',
-              //   postPublishDate: '2',
-              //   postDistance: '1',
-              //   postDescription: 'Post Dddddescrisssption',
-              // ),
-              // SizedBox(height: 3),
-              // SmallPost(
-              //   postTitle: 'Post Title',
-              //   postImage: 'https://i.pravatar.cc/303',
-              //   postAuthorName: 'John Doe',
-              //   postPublishDate: '2',
-              //   postDistance: '1',
-              //   postDescription:
-              //       'Post https://pub.dev/packages/expandable_text Dddddescrdfffffffffffffffdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfffffffffffffffffffffffffffffffffffffffffisf dddddddddddsdffffffffffffsdfffffdddption',
-              // ),
-              // SizedBox(height: 3),
-              // SmallPost(
-              //   postTitle:
-              //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-              //   postImage: 'https://i.pravatar.cc/303',
-              //   postAuthorName: 'John Doe',
-              //   postPublishDate: '2',
-              //   // postCategory: Category.warnung,
-              //   postDistance: '1',
-              //   postDescription:
-              //       'Post https://pub.dev/packages/expandable_text Dddddescrdfffffffffffffffdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfffffffffffffffffffffffffffffffffffffffffisf dddddddddddsdffffffffffffsdfffffdddption',
-              // ),
+              Container(
+                child: StreamBuilder<List<models.Post>>(
+                  stream: dataAccess.getPosts(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Text(
+                          'Something went wrong: ${snapshot.error.toString()}',
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                              fontSize: 32, fontWeight: FontWeight.w300));
+                    } else if (snapshot.hasData) {
+                      final posts = snapshot.data!;
+
+                      return ListView.separated(
+                        shrinkWrap: true,
+                        itemCount: posts.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final post = posts.elementAt(index);
+
+                          return FutureBuilder<models.User?>(
+                            future: dataAccess.getUser(post.user),
+                            builder: ((context, snapshot) {
+                              if (snapshot.hasData) {
+                                final user = snapshot.data!;
+                                return Post(
+                                  post: post,
+                                  postAuthorName:
+                                      '${user.firstName} ${user.lastName}',
+                                  postAuthorImage: user.imageUrl,
+                                );
+                              } else {
+                                return Container();
+                              }
+                            }),
+                          );
+                        },
+                        separatorBuilder: (BuildContext context, int index) =>
+                            const SizedBox(height: 3),
+                      );
+                    } else {
+                      return const Center(child: CircularProgressIndicator());
+                      // return const Text('There are no posts in the moment',
+                      //   textAlign: TextAlign.center,
+                      //   style: TextStyle(fontSize: 32, fontWeight: FontWeight.w300));
+                    }
+                  },
+                ),
+              ),
             ]),
           ),
         ],
