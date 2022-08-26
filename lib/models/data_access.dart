f10b12f75a2316f67dfafca769e04fb0168ef14f
sandin <sandin-habibovic@hotmail.com>
2022-08-26 16:34:50 +0200

    Update data_access.dart

--- lib/models/data_access.dart
+++ lib/models/data_access.dart
@@ -7,6 +7,8 @@ import 'package:get/get.dart';
 import 'package:locoo/models/category.dart';
 import 'package:locoo/models/post.dart';
 import 'package:locoo/models/user.dart' as models;
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+
 
 class DataAccess extends GetxService {
     final CollectionReference<Map<String, dynamic>> userdataCol = FirebaseFirestore.instance.collection('users');
@@ -53,6 +55,21 @@ class DataAccess extends GetxService {
 
       if(snapshot.exists) {
         return models.User.fromJson(snapshot.data()!);
+      } else {
+        return null;
+      }
+    } on Exception catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<types.User?> getChatUser(String uid) async {
+    try {
+      final snapshot = await userdataCol.doc(uid).get();
+
+      if(snapshot.exists) {
+        return types.User.fromJson(snapshot.data()!);
       } else {
         return null;
       }
