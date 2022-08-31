23acd8b0745e681e1eac0948bb809b1000c655e1
Martin <s.m@hausleitner.eu>
2022-08-31 14:42:59 +0200

    update

--- lib/pages/feed/feed_page.dart
+++ lib/pages/feed/feed_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:locoo/models/category.dart';
 import 'package:locoo/models/data_access.dart';
@@ -14,50 +15,56 @@ class FeedPage extends GetView<FeedController> {
   Widget build(BuildContext context) {
     final dataAccess = Get.find<DataAccess>();
     return Scaffold(
-      body: Container(
-        child: StreamBuilder<List<models.Post>>(
-          stream: dataAccess.getPosts(),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Text('Something went wrong: ${snapshot.error.toString()}',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                      fontSize: 32, fontWeight: FontWeight.w300));
-            } else if (snapshot.hasData) {
-              final posts = snapshot.data!;
+     
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.dark,
+        child: Container(
+          child: StreamBuilder<List<models.Post>>(
+            stream: dataAccess.getPosts(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Text(
+                    'Something went wrong: ${snapshot.error.toString()}',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        fontSize: 32, fontWeight: FontWeight.w300));
+              } else if (snapshot.hasData) {
+                final posts = snapshot.data!;
 
-              return ListView.separated(
-                shrinkWrap: true,
-                itemCount: posts.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final post = posts.elementAt(index);
+                return ListView.separated(
+                  shrinkWrap: true,
+                  itemCount: posts.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final post = posts.elementAt(index);
 
-                  return FutureBuilder<models.User?>(
-                    future: dataAccess.getUser(post.user),
-                    builder: ((context, snapshot) {
-                      if (snapshot.hasData) {
-                        final user = snapshot.data!;
-                        return Post(
-                          post: post,
-                          postAuthorName: '${user.firstName} ${user.lastName}',
-                          postAuthorImage: user.imageUrl,
-                        );
-                      } else {
-                        return Container();
-                      }
-                    }),
-                  );
-                },
-                separatorBuilder: (BuildContext context, int index) =>
-                    const SizedBox(height: 3),
-              );
-            } else {
-              return const Center(child: CircularProgressIndicator());
-              // return const Text('There are no posts in the moment',
-              //   textAlign: TextAlign.center,
-              //   style: TextStyle(fontSize: 32, fontWeight: FontWeight.w300));
-            }
-          },
+                    return FutureBuilder<models.User?>(
+                      future: dataAccess.getUser(post.user),
+                      builder: ((context, snapshot) {
+                        if (snapshot.hasData) {
+                          final user = snapshot.data!;
+                          return Post(
+                            post: post,
+                            postAuthorName:
+                                '${user.firstName} ${user.lastName}',
+                            postAuthorImage: user.imageUrl,
+                          );
+                        } else {
+                          return Container();
+                        }
+                      }),
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const SizedBox(height: 3),
+                );
+              } else {
+                return const Center(child: CircularProgressIndicator());
+                // return const Text('There are no posts in the moment',
+                //   textAlign: TextAlign.center,
+                //   style: TextStyle(fontSize: 32, fontWeight: FontWeight.w300));
+              }
+            },
+          ),
         ),
       ),
     );
