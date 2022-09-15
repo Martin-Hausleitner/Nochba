0073789530273fdf4d07cf78561d5d45cdc84cb8
Edalatkhah <arsham.edal@gmail.com>
2022-09-15 15:33:35 +0200

    Chat update

---
+++ lib/models/src/flutter_chat_types.dart
@@ -0,0 +1,16 @@
+library flutter_chat_types;
+
+export 'src/message.dart';
+export 'src/messages/custom_message.dart';
+export 'src/messages/file_message.dart';
+export 'src/messages/image_message.dart';
+export 'src/messages/partial_custom.dart';
+export 'src/messages/partial_file.dart';
+export 'src/messages/partial_image.dart';
+export 'src/messages/partial_text.dart';
+export 'src/messages/system_message.dart';
+export 'src/messages/text_message.dart';
+export 'src/messages/unsupported_message.dart';
+export 'src/preview_data.dart';
+export 'src/room.dart';
+export 'src/user.dart';
