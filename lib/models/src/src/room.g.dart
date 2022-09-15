aeb6c01acdeeaedf37843450e0c89ae9dc66964c
Edalatkhah <arsham.edal@gmail.com>
2022-09-15 15:17:57 +0200

    Auto stash before merge of "main" and "origin/main"

---
+++ lib/models/src/src/room.g.dart
@@ -0,0 +1,51 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'room.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Room _$RoomFromJson(Map<String, dynamic> json) => Room(
+      createdAt: json['createdAt'] as int?,
+      id: json['id'] as String,
+      imageUrl: json['imageUrl'] as String?,
+      lastMessages: (json['lastMessages'] as List<dynamic>?)
+          ?.map((e) => Message.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      metadata: json['metadata'] as Map<String, dynamic>?,
+      name: json['name'] as String?,
+      type: $enumDecodeNullable(_$RoomTypeEnumMap, json['type']),
+      updatedAt: json['updatedAt'] as int?,
+      users: (json['users'] as List<dynamic>)
+          .map((e) => User.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$RoomToJson(Room instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('createdAt', instance.createdAt);
+  val['id'] = instance.id;
+  writeNotNull('imageUrl', instance.imageUrl);
+  writeNotNull(
+      'lastMessages', instance.lastMessages?.map((e) => e.toJson()).toList());
+  writeNotNull('metadata', instance.metadata);
+  writeNotNull('name', instance.name);
+  writeNotNull('type', _$RoomTypeEnumMap[instance.type]);
+  writeNotNull('updatedAt', instance.updatedAt);
+  val['users'] = instance.users.map((e) => e.toJson()).toList();
+  return val;
+}
+
+const _$RoomTypeEnumMap = {
+  RoomType.channel: 'channel',
+  RoomType.direct: 'direct',
+  RoomType.group: 'group',
+};
