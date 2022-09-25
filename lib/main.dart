62d11a6f6631cb40ce814f4dd46f7db791e7213e
Martin <s.m@hausleitner.eu>
2022-09-25 19:51:30 +0200

    app check

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'routes/app_pages.dart';
 import 'routes/app_routes.dart';
@@ -10,10 +9,16 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:json_theme/json_theme.dart';
 import 'firebase_options.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // await FirebaseAppCheck.instance.activate(
+  //     // webRecaptchaSiteKey: 'recaptcha-v3-site-key',  // If you're building a web app.
+  //     );
 
   final themeStr = await rootBundle.loadString('assets/appainter_theme.json');
   final themeJson = jsonDecode(themeStr);
