26d29befdc85cc451314009ddc39b5e6fa1e8dc2
Martin <s.m@hausleitner.eu>
2022-09-28 17:55:16 +0200

    import fix

--- lib/pages/feed/views/feed_post_filter_view.dart
+++ lib/pages/feed/views/feed_post_filter_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_remix/flutter_remix.dart';
-import 'package:locoo/pages/feed/post/widgets/filter/filter_range_slider.dart';
-import 'package:locoo/pages/feed/post/widgets/filter/filter_title.dart';
+import 'package:locoo/pages/feed/widgets/filter/filter_range_slider.dart';
+import 'package:locoo/pages/feed/widgets/filter/filter_title.dart';
 import 'package:locoo/shared/ui/buttons/locoo_circular_icon_button.dart';
 import 'package:locoo/shared/ui/buttons/locoo_text_button.dart';
 import 'package:locoo/shared/views/bottom_sheet_title_close_view.dart';
