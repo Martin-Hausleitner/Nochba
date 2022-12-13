acdcd6a839c6c83d7400872e4bcdf549e5ddce58
Martin <s.m@hausleitner.eu>
2022-12-13 15:14:11 +0100

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,28 @@
 import * as functions from "firebase-functions";
 import { Client, GeocodeResponse } from "@googlemaps/google-maps-services-js";
+import * as admin from 'firebase-admin';
 
+
+function getDistanceFromLatLonInMeters(
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number
+): number {
+  const R = 6371e3; // radius of Earth in meters
+  const φ1 = lat1 * (Math.PI / 180); // convert lat1 to radians
+  const φ2 = lat2 * (Math.PI / 180); // convert lat2 to radians
+  const Δφ = (lat2 - lat1) * (Math.PI / 180); // difference in latitudes, converted to radians
+  const Δλ = (lon2 - lon1) * (Math.PI / 180); // difference in longitudes, converted to radians
+
+  const a =
+    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  const d = R * c; // distance in meters
+  return d;
+}
 //firebase emulators:start
 
 // // Start writing functions
@@ -48,6 +70,11 @@ export const checkAddressWithDeviceLocation = functions
       );
       console.log("Distance:" + distance);
       if (distance < 40) {
+        //write a new firestrore query which saves a geo cordiandte to the users /uid userInternInfo doc as location
+
+      
+        
+        
         return {
           success: true,
           // return distance
@@ -116,26 +143,5 @@ export const checkAddressWithDeviceLocation = functions
 
       // // Return the coordinates as a Coordinates object
       // return { latitude: lat, longitude: lng };
-    }
-
-    function getDistanceFromLatLonInMeters(
-      lat1: number,
-      lon1: number,
-      lat2: number,
-      lon2: number
-    ): number {
-      const R = 6371e3; // radius of Earth in meters
-      const φ1 = lat1 * (Math.PI / 180); // convert lat1 to radians
-      const φ2 = lat2 * (Math.PI / 180); // convert lat2 to radians
-      const Δφ = (lat2 - lat1) * (Math.PI / 180); // difference in latitudes, converted to radians
-      const Δλ = (lon2 - lon1) * (Math.PI / 180); // difference in longitudes, converted to radians
-
-      const a =
-        Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-        Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-      const d = R * c; // distance in meters
-      return d;
     }
   });
