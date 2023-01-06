de26d57569897e3a3a4d793dfeb2631bcec4199e
Martin <s.m@hausleitner.eu>
2023-01-06 04:04:09 +0100

    icon update

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,7 +6,7 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <connectivity_plus_windows/connectivity_plus_windows_plugin.h>
+#include <connectivity_plus/connectivity_plus_windows_plugin.h>
 #include <geolocator_windows/geolocator_windows.h>
 #include <image_compression_flutter/image_compression_flutter_plugin.h>
 #include <nb_utils/nb_utils_plugin.h>
