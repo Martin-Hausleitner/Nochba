b799792af3cc9b63e45bccc2566983771773c72f
sandin <sandin-habibovic@hotmail.com>
2023-01-09 13:39:59 +0100

    test cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,7 +7,7 @@ import * as admin from "firebase-admin";
 // firebase functions:secrets:set GOOGLE_MAPS_API_KEY
 
 // Initialize Firebase app
-  admin.initializeApp();
+admin.initializeApp();
 
 
 
@@ -15,6 +15,7 @@ export { generateVerificationCode } from "./cloudFunctions/generateVerificationC
 export { checkAddressWithDeviceLocation } from "./cloudFunctions/checkAddressWithDeviceLocation";
 export { getDistanceFromTwoUsers } from "./cloudFunctions/getDistanceFromTwoUsers";
 export { checkVerificationCode } from "./cloudFunctions/checkVerificationCode";
+export { createUser } from "./cloudFunctions/createUser";
 // Get a reference to the Firestore database
 //firebase emulators:start
 // // Start writing functions
