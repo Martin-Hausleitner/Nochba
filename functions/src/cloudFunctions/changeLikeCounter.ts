b9c55c2be011accbd5a933e5ba25e941dd76dbaf
Martin <s.m@hausleitner.eu>
2023-01-29 18:47:58 +0100

    Update changeLikeCounter.ts

--- functions/src/cloudFunctions/changeLikeCounter.ts
+++ functions/src/cloudFunctions/changeLikeCounter.ts
@@ -3,30 +3,32 @@ import * as admin from "firebase-admin";
 
 var db = admin.firestore();
 
-export const incrementLikeCounter = functions.region('europe-west1').firestore
-    .document('users/{userId}/private/record/likedPosts/{likedPostId}')
-    .onCreate(async (snapshot, context) => {
-        const likedPostId = context.params.likedPostId;
+export const incrementLikeCounter = functions
+  .region("europe-west1")
+  .firestore.document("users/{userId}/private/record/likedPosts/{likedPostId}")
+  .onCreate(async (snapshot, context) => {
+    const likedPostId = context.params.likedPostId;
 
-        const likedPost = db.collection('posts').doc(likedPostId);
+    const likedPost = db.collection("posts").doc(likedPostId);
 
-        await db.runTransaction(async (transaction) => {
-            const snapshot = await transaction.get(likedPost);
-            const newLikeCounter = snapshot.get("likes") + 1;
-            transaction.update(likedPost, { "likes": newLikeCounter });
-        });
+    await db.runTransaction(async (transaction) => {
+      const snapshot = await transaction.get(likedPost);
+      const newLikeCounter = snapshot.get("likes") + 1;
+      transaction.update(likedPost, { likes: newLikeCounter });
     });
+  });
 
-export const decrementLikeCounter = functions.firestore
-    .document('users/{userId}/private/record/likedPosts/{likedPostId}')
-    .onDelete(async (snapshot, context) => {
-        const likedPostId = context.params.likedPostId;
+export const decrementLikeCounter = functions
+  .region("europe-west1")
+  .firestore.document("users/{userId}/private/record/likedPosts/{likedPostId}")
+  .onDelete(async (snapshot, context) => {
+    const likedPostId = context.params.likedPostId;
 
-        const likedPost = db.collection('posts').doc(likedPostId);
+    const likedPost = db.collection("posts").doc(likedPostId);
 
-        await db.runTransaction(async (transaction) => {
-            const snapshot = await transaction.get(likedPost);
-            const newLikeCounter = snapshot.get("likes") - 1;
-            transaction.update(likedPost, { "likes": newLikeCounter });
-        });
+    await db.runTransaction(async (transaction) => {
+      const snapshot = await transaction.get(likedPost);
+      const newLikeCounter = snapshot.get("likes") - 1;
+      transaction.update(likedPost, { likes: newLikeCounter });
     });
+  });
